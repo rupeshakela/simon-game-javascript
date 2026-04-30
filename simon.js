aa8b49ae92fa0a1f297d7b9b7eb6727c7e211005
Rupesh Kumar <rayr51488@gmail.com>
2026-04-30 22:44:07 +0530

    Refactor Simon game logic and user input handling
    Refactor game logic and improve user interaction.

--- simon.js
+++ simon.js
@@ -1,78 +1,101 @@
-let gameSeq=[];
-let userSeq=[];
+let gameSeq = [];
+let userSeq = [];
 
-let btns=["red","yellow","green","purple"];
+let btns = ["red", "yellow", "green", "purple"];
 
-let started=false;
-let level=0;
+let started = false;
+let level = 0;
 
-let h2=document.querySelector("h2");
+let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress",function(){
-    if(started==false){
+document.addEventListener("keypress", function () {
+    if (started == false) {
         console.log("game is started");
-        started=true;
+        started = true;
         levelup();
     }
 });
-function gameFlash(btn){
+
+function gameFlash(btn) {
     btn.classList.add("flash");
-    setTimeout(function(){
+
+    setTimeout(function () {
         btn.classList.remove("flash");
-    },250);
+    }, 250);
 }
 
-function userFlash(btn){
+function userFlash(btn) {
     btn.classList.add("userflash");
-    setTimeout(function(){
-        btn.classList.remove("userflash");
-    },250);
-}
-function levelup(){
-    userSeq=[];
-    level++;
-    h2.innerText=`level ${level}`;
 
-    let randIdx=Math.floor(Math.random()*4);
-    let randColor=btns[randIdx];
-    let randbtn=document.querySelector(`.${randColor}`);
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 250);
+}
+
+function levelup() {
+    userSeq = [];
+    level++;
+
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 4);
+    let randColor = btns[randIdx];
+    let randbtn = document.querySelector(`.${randColor}`);
+
     gameSeq.push(randColor);
     console.log(gameSeq);
+
     gameFlash(randbtn);
 }
 
-function checkAns(idx){
-    if(userSeq[idx]===gameSeq[idx]){
-        if(userSeq.length==gameSeq.length){
-            setTimeout(levelup,1000);
+function checkAns(idx) {
+    if (userSeq[idx] === gameSeq[idx]) {
+
+        if (userSeq.length == gameSeq.length) {
+            setTimeout(levelup, 1000);
         }
-    }else{
-        h2.innerHTML=`Game Over!your score was <b>${level}</b> <br>Press any key to start`;
-        document.querySelector("body").style.backgroundColor="red";
-        setTimeout(function(){
-            document.querySelector("body").style.backgroundColor="white";
-        },150);
+
+    } else {
+
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br>Press any key to start`;
+
+        document.querySelector("body").style.backgroundColor = "red";
+
+        setTimeout(function () {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+
         reset();
     }
 }
 
-function btnPress(){
+function btnPress() {
+
+    // game start hone se pehle click ignore hoga
+    if (started == false) {
+        return;
+    }
+
     console.log(this);
-    let btn=this;
+
+    let btn = this;
     userFlash(btn);
 
-    let userColor=btn.getAttribute("id");
+    let userColor = btn.getAttribute("id");
     userSeq.push(userColor);
-    checkAns(userSeq.length-1);
-}
-let allbtns=document.querySelectorAll(".btn");
-for(let btn of allbtns){
-    btn.addEventListener("click",btnPress);
+
+    checkAns(userSeq.length - 1);
 }
 
-function reset(){
-    started=false;
-    gameSeq=[];
-    userSeq=[];
-    level=0;
+let allbtns = document.querySelectorAll(".btn");
+
+for (let btn of allbtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }
